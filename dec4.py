63d2691a6f07a76f4c08f19282d41253cee0291c
Blom <blom.beats@gmail.com>
2021-12-06 13:32:00 +0100

    part1 done

--- dec4.py
+++ dec4.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def convertNpDecimal(arr: np.ndarray):
     return int("".join([str(x) for x in arr]), 2)
 
@@ -8,23 +9,109 @@ def loadData():
     with open('dec4_input.txt', "r") as f:
         data = f.readlines()
 
-    data = [x[:-1] for x in data]  # remove endline (there's a better way to do this, but I'm lazy)
+    data = [x.replace("\n","") for x in data]  # remove endline (there's a better way to do this, but I'm lazy)
 
-    drawOrder=data.pop(0)
-    drawOrder=drawOrder.split(",")
-    drawOrder=[int(x) for x in drawOrder]
+    drawOrder = data.pop(0)
+    drawOrder = drawOrder.split(",")
+    drawOrder = np.array([int(x) for x in drawOrder])
 
+    boards = []
 
-    boards=[]
+    for i in range(0, len(data), 6):
 
-    for i in range(0,len(data),6):
-        newBoard=np.zeros([5,5],dtype=int)
+        currentBoard = data[i:i + 6]
+        currentBoard = [[int(y) for y in x.split()] for x in currentBoard if x]
 
-        for y in range(1,6): #Skip first one as it's empty
-            newBoard[y]=data[i+y].split()
+        newBoard = np.zeros([5, 5], dtype=int)
+
+        for y in range(len(currentBoard)):  # Skip first one as it's empty
+            newBoard[y] = currentBoard[y]
 
         boards.append(newBoard)
 
-    return drawOrder,boards
+    return drawOrder, np.array(boards)
 
-drawOrder,boards=loadData()
+
+def checkBingo(stats):
+    bingo = False
+
+    # Check rows
+    for i in range(len(stats)):
+        if stats[i].all() == True:
+            bingo = True
+            break
+
+    # Columns
+    for i in range(len(stats)):
+        if stats[:, i].all() == True:
+            bingo = True
+            break
+
+    return bingo
+
+
+def calcBoard(board, drawOrder):
+    """
+
+    :param board:
+    :param drawOrder:
+    :return: number of draws it took to win and if won att all
+    """
+
+    bingo = False
+
+    stats = np.zeros([5, 5], dtype=bool)
+    """
+        Show 1 if its been drawn at that location
+        Show 0 if not drawn
+    """
+    draw = 0  # IDE complained on me
+
+    for draw in range(len(drawOrder)):
+        # print(draw)
+
+        stats += board == drawOrder[draw]  # adds all true to the stats
+
+        bingo = checkBingo(stats)
+        if bingo:
+            break
+    if bingo:
+
+        return np.array([draw, board, stats,drawOrder[draw]])
+    else:
+        return False
+
+
+def playGames(boards, drawOrder):
+    # Play all games
+    games = []
+    for b in boards:
+        games.append(calcBoard(b, drawOrder))
+
+    # Filter games
+    games = [x for x in games if x is not False]
+    print(f"Won Games: {len(games)}")
+
+    bestGame = min([x[0] for x in games])
+
+    bestGames = [x for x in games if x[0] == bestGame]
+
+    if len(bestGames) > 1:
+        raise ValueError("nono seniior")
+
+    return bestGames[0]
+
+
+def calcPoints(game):
+    unmarkedSum = game[1][np.invert(game[2])].sum()
+    lastCalled = game[3]
+    return unmarkedSum * lastCalled
+
+
+drawOrder, boards = loadData()
+
+game = playGames(boards, drawOrder) #Best game
+
+score=calcPoints(game)
+
+print(f"Score: {score}")
