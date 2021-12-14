f5a0aebd090157046cd7da425d45c1ee19fb32a1
blombeats <51360317+blombeats@users.noreply.github.com>
2021-12-14 22:40:18 +0100

    Add files via upload
    part 1 dec5 done

--- dec5.py
+++ dec5.py
@@ -1,31 +1,64 @@
 import numpy as np
 
-def loadData():
-    with open('dec4_input.txt', "r") as f:
-        data = f.readlines()
+def loadData(filename):
+    with open(filename,"r") as f:
+        data=f.readlines()
 
-    data = [x.replace("\n","") for x in data]  # remove endline (there's a better way to do this, but I'm lazy)
+    data=[x.replace("\n","") for x in data]
 
     return data
 
-def processData(data):
+
+data=loadData('dec5_test.txt')
+data=loadData('dec5_input.txt')
+
+data=np.array([[[int(z) for z in y.split(",")] for y in x.split(" -> ")] for x in data])
+
+len(data)
+#x1,y1 x2,y2
 
 
-data=loadData()
+# filter to only horizontal and vertical lines
+cords=np.array([x for x in data if x[0][0] == x[1][0] or x[0][1] == x[1][1]])
+
+print(f"data: {len(data)}")
+print(f"cords: {len(cords)}")
+
+#get max x y cordinates
+cordsMax=np.max(cords)
+print(f"CordsMax: {cordsMax}")
 
 
-drawOrder = np.array([int(x) for x in data])
+# create map (zeros)
+myMap=np.zeros([cordsMax+1,cordsMax+1])
+print(f"mymap shape {myMap.shape}")
 
-    boards = []
 
-    for i in range(0, len(data), 6):
+# draw lines
 
-        currentBoard = data[i:i + 6]
-        currentBoard = [[int(y) for y in x.split()] for x in currentBoard if x]
 
-        newBoard = np.zeros([5, 5], dtype=int)
+for x in cords:
+    
+    #The smallest value in the right place
+    if x[0][0]<x[1][0]:
+        x1=x[0][0]
+        x2=x[1][0]
+    else:
+        x1=x[1][0]
+        x2=x[0][0]
+    
+    if x[0][1]<x[1][1]:
+        y1=x[0][1]
+        y2=x[1][1]
+    else:
+        y1=x[1][1]
+        y2=x[0][1]
+    
+    
+    myMap[y1:y2+1,x1:x2+1]+=1
 
-        for y in range(len(currentBoard)):  # Skip first one as it's empty
-            newBoard[y] = currentBoard[y]
 
-        boards.append(newBoard)
+
+result=(myMap >= 2).sum()
+
+print(f"Result: {result}")
